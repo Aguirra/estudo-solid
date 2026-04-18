82fadf5148539c337d5d4231cd8d713cb07c0d48
Ricardo Aguirra <ramenendes@gmail.com>
2026-04-18 11:06:04 -0300

    Envio SOLID-ISP

---
+++ src/com/solid/ISP/IspMain.java
@@ -0,0 +1,29 @@
+package com.solid.ISP;
+
+public class LspMain {
+
+    public static void main(String[] args) {
+
+        Pessoa pessoaFisica = new PessoaFisica(
+                "Paciente Teste", "123.456.789-00", "01/01/1990");
+
+        Pessoa pessoaJuridica = new PessoaJuridica(
+                "Laboratorio XYZ", "12.345.678/0001-99");
+
+        System.out.println("Pessoa Física:");
+        System.out.println("Nome: " + pessoaFisica.getNome());
+        System.out.println("Documento: " + pessoaFisica.getDocumentoPrincipal());
+
+
+        if (pessoaFisica instanceof PessoaFisica pf) {
+            System.out.println("Data de nascimento: " + pf.getDataNascimento());
+        }
+
+        System.out.println("\nPessoa Jurídica:");
+        System.out.println("Nome: " + pessoaJuridica.getNome());
+        System.out.println("Documento: " + pessoaJuridica.getDocumentoPrincipal());
+    }
+}
+
+
+
